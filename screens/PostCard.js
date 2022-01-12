8558f412cd476f626386ce81afd93e898f9339d8
ManyaSharma101007 <73381360+ManyaSharma101007@users.noreply.github.com>
2022-01-12 12:49:17 +0530

    Update PostCard.js

--- screens/PostCard.js
+++ screens/PostCard.js
@@ -1,10 +1,23 @@
-import {React} from 'react';
-import {View,Text,RFValue,StyleSheet} from 'react-native';
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+} from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import { RFValue } from "react-native-responsive-fontsize";
 
-export default class PostCard extends React.Componennt {
+export default class PostCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() { }
+
     render() {
-        return(
+        return (
             <View style={styles.container}>
                 <View style={styles.cardContainer}>
                     <View style={styles.authorContainer}>
@@ -12,46 +25,80 @@ export default class PostCard extends React.Componennt {
                             <Image
                                 source={require("../assets/profile_img.png")}
                                 style={styles.profileImage}
-                            />
+                            ></Image>
                         </View>
                         <View style={styles.authorNameContainer}>
-                            <Text style={styles.authorNameText}> {this.props.post.author} </Text>
+                            <Text style={styles.authorNameText}>{this.props.post.author}</Text>
                         </View>
                     </View>
-                    <Image source={require("../assets/post.jpeg")} styles={styles.postImage}/>
+                    <Image source={require("../assets/post.jpeg")} style={styles.postImage} />
                     <View style={styles.captionContainer}>
-                        <Text style={styles.captionText}> {this.props.post.caption} </Text>
+                        <Text style={styles.captionText}>
+                            {this.props.post.caption}
+                        </Text>
                     </View>
                     <View style={styles.actionContainer}>
                         <View style={styles.likeButton}>
                             <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
-                            <Text style={styles.likeText}> 12k </Text>
+                            <Text style={styles.likeText}>12k</Text>
                         </View>
                     </View>
                 </View>
             </View>
-        )
+        );
     }
 }
 
 const styles = StyleSheet.create({
-    container : {
-        flex : 1,
-        backgroundColor :  "black"
+    container: {
+        flex: 1
     },
-    cardContainer : {
-        flex : 0.85 
+    cardContainer: {
+        margin: RFValue(13),
+        backgroundColor: "#2a2a2a",
+        borderRadius: RFValue(20),
+        padding: RFValue(20)
+    },
+    authorContainer: {
+        flex: 0.1,
+        flexDirection: "row"
+    },
+    authorImageContainer: {
+        flex: 0.15,
+        justifyContent: "center",
+        alignItems: "center"
     },
     profileImage: {
-        width: RFValue(140),
-        height: RFValue(140),
-        borderRadius: RFValue(70)
+        width: "100%",
+        height: "100%",
+        resizeMode: "contain",
+        borderRadius: RFValue(100)
     },
-    likeText: {
+    authorNameContainer: {
+        flex: 0.85,
+        justifyContent: "center"
+    },
+    authorNameText: {
         color: "white",
-        fontFamily: "Bubblegum-Sans",
-        fontSize: RFValue(25),
-        marginLeft: RFValue(5)
+        fontSize: RFValue(20)
+    },
+    postImage: {
+        marginTop: RFValue(20),
+        resizeMode: "contain",
+        width: "100%",
+        alignSelf: "center",
+        height: RFValue(275)
+    },
+    captionContainer: {},
+    captionText: {
+        fontSize: 13,
+        color: "white",
+        paddingTop: RFValue(10)
+    },
+    actionContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        padding: RFValue(10)
     },
     likeButton: {
         width: RFValue(160),
@@ -62,26 +109,9 @@ const styles = StyleSheet.create({
         backgroundColor: "#eb3948",
         borderRadius: RFValue(30)
     },
-    actionContainer: {
-        justifyContent: "center",
-        alignItems: "center",
-        padding: RFValue(10)
-    },
-    authorNameText: {
-        fontFamily: "Bubblegum-Sans",
-        fontSize: RFValue(18),
-        color: "white"
-    },
-    authorNameContainer: {
-        padding: RFValue(20)
-    },
-    captionContainer: {
-        flex: 0.5,
-        justifyContent: "center"
-    },  
-    captionText: {
+    likeText: {
         color: "white",
-        fontSize: RFValue(18),
-        fontFamily: "Bubblegum-Sans"
-    },
-})
+        fontSize: RFValue(25),
+        marginLeft: RFValue(5)
+    }
+});
